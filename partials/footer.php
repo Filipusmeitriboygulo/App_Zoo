2f0c45e1f6ce8f29676f4ad29358659358a94ccf
Filipusmeitriboygulo <filipusmeitriboygulo@gmail.com>
2024-06-25 13:51:55 +0700

    first commit

---
+++ partials/footer.php
@@ -0,0 +1,94 @@
+<!-- Remove the container if you want to extend the Footer to full width. -->
+<div class="mt-5">
+
+    <footer class="text-white text-center text-lg-start" style="background-color: #373A40;">
+        <!-- Grid container -->
+        <div class=" container p-4">
+            <!--Grid row-->
+            <div class="row mt-4">
+                <!--Grid column-->
+                <div class="col-lg-4 col-md-12 mb-4 mb-md-0" style="font-size: small;">
+                    <h4 class=" mb-4">Hubungi Kami</h4>
+                    <p>
+                        Telp: 0274-373861
+                    </p>
+                    <p>
+                        Lokasi : Jl. Medan - Banda Aceh
+                    </p>
+
+                    <p>
+                        Fax : 0274-384666
+
+                    </p>
+                    <p>
+                        Email : info@gembiralokazoo.com
+                    </p>
+
+                    <div class="mt-4">
+                        <!-- Facebook -->
+                        <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-facebook-f"></i></a>
+                        <!-- Dribbble -->
+                        <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-dribbble"></i></a>
+                        <!-- Twitter -->
+                        <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-twitter"></i></a>
+                        <!-- Google + -->
+                        <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-google-plus-g"></i></a>
+                        <!-- Linkedin -->
+                    </div>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-4 col-md-6 mb-4 mb-md-0" style="font-size: small;">
+                    <h4>
+                        Tentang Kami
+                    </h4>
+                    <p>
+                        Fasilitas Umum
+                    </p>
+                    <p>
+                        Lembaga Konservasi
+                    </p>
+
+                    <p>
+                        Kebijakan Privasi
+
+                    </p>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+
+                <div class="col-lg-4 col-md-6 mb-4 mb-md-0" style="font-size: small;">
+                    <h5 class="text-uppercase mb-4">Opening hours</h5>
+
+                    <table class="table text-center text-white">
+                        <tbody class="fw-normal">
+                            <tr>
+                                <td>Monday - Friday:</td>
+                                <td>08:30 - 17:00</td>
+                            </tr>
+                            <tr>
+                                <td>Satuday - Sunday:</td>
+                                <td>08:00 - 18:00</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <!--Grid column-->
+            </div>
+            <!--Grid row-->
+        </div>
+        <!-- Grid container -->
+
+        <!-- Copyright -->
+        <div class="text-end p-3" style="background-color: #373A40;">
+            © 2024 Boy&Friend Dev:
+            <a class="text-white me-2" href="https://heroic-pony-19f6e8.netlify.app/">Boy&FriendDev.com</a>
+            <img class="m-2" src="assets/img/logo.jpg" height="70px" width="70px" alt="" />
+        </div>
+        <!-- Copyright -->
+    </footer>
+
+</div>
+<!-- End of .container -->
---
+++ partials/navbar.php
@@ -0,0 +1,37 @@
+<section class=" d-flex justify-content-center">
+
+    <!-- Navbar -->
+    <nav class="container-lg navbar navbar-expand-lg bg-light fs-6 font position-fixed shadow p-2 mb-2 py-0 bg-body-tertiary rounded">
+        <div class="container">
+            <a class="navbar-brand fs-5 font" href="#"><img class="m-2" src="assets/img/logo.jpg" height="70px" width="70px" alt="" />B-ZOO</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item px-3">
+                        <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+                    </li>
+                    <li class="nav-item px-3">
+                        <a class="nav-link" href="#">Berita</a>
+                    </li>
+                    <li class="nav-item px-3">
+                        <a class="nav-link" href="#">Profil</a>
+                    </li>
+                    <li class="nav-item px-3">
+                        <a class="nav-link" href="#">Satwa</a>
+                    </li>
+                    <li class="nav-item px-3">
+                        <a class="nav-link" href="#">Peta</a>
+                    </li>
+                    <li class="nav-item px-3">
+                        <a class="nav-link" href="#">Kontak</a>
+                    </li>
+                    <li class="nav-item px-3">
+                        <button type="button" class="btn btn-warning">Tiket</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</section>
